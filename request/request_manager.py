25ae7ae08e64922037f4aa535bd719da80b27d90
Nick <65495045+NickKoepr@users.noreply.github.com>
2022-10-09 17:27:39 +0200

    Added more comment and added a new function in request_manager.py

--- request/request_manager.py
+++ request/request_manager.py
@@ -8,8 +8,8 @@ def create_invite(invited_id: int, inviter_id: int, message_id: int):
     """
     Send an invitation to a user for a game.
 
-    :param invited_id: The user ID of the inviter.
-    :param inviter_id: The user ID of the member that's invited.
+    :param invited_id: The user ID of the invited member.
+    :param inviter_id: The user ID of the inviter.
     :param message_id: The message ID of the message.
     """
 
@@ -30,6 +30,15 @@ def has_open_request(inviter_id: int):
 
 
 def try_accepting_request(invited_id: int, message_id: int):
+    """
+    Try accepting an invite request with the invited_id and the message_id.
+
+    :param invited_id: The user ID of the invited member.
+    :param message_id: The message ID of the message.
+    :return: False when there is no request to accept, otherwise return a list with the accepted request as the first
+    element and a list with declined requests as a second element.
+    """
+
     decline_requests = []
     if invited_id in invited_users.keys():
         # Get all the invites that the user received (invited_id)
@@ -48,8 +57,10 @@ def try_accepting_request(invited_id: int, message_id: int):
 
                 # Check if the invited person has sent a game request to a player.
                 if invited_id in inviter_users:
+                    # Go over all invites and check if the invited id is equal to the inviter_id.
                     for inv in invited_users[inviter_users[invited_id].invited_id]:
                         if inv.inviter_id == invited_id:
+                            # When true, add the request to the decline_requests.
                             decline_requests.append(inv)
                             all_inv = invited_users[inv.invited_id]
                             inviter_users.pop(invited_id)
@@ -58,7 +69,9 @@ def try_accepting_request(invited_id: int, message_id: int):
                             else:
                                 all_inv.remove(inv)
 
+                # Remove the inviter from the inviter_list.
                 inviter_users.pop(inviter_id)
+                # If the invited_users list contains inviter_id, remove the inviter_id from that list.
                 if inviter_id in invited_users:
                     inviter_invites = invited_users[inviter_id]
                     for inviter_invite in inviter_invites:
@@ -67,5 +80,17 @@ def try_accepting_request(invited_id: int, message_id: int):
                     invited_users.pop(inviter_id)
 
             return [invite, decline_requests]
-
     return False
+
+
+def has_sent_an_invite(inviter_id: int):
+    """
+    Return False when the given member ID has an open invite request, otherwise return True.
+
+    :param inviter_id: The user ID of the inviter.
+    :return: True or False
+    """
+    if inviter_id in inviter_users.keys():
+        return True
+    else:
+        return False
